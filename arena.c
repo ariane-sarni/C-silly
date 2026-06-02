017ae0d5abdc34d26da6c187aee7103e1c08f386
ariane-sarni <arianesarni@gmail.com>
2026-06-02 14:29:22 -0700

    Still messing around with C

--- arena.c
+++ arena.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+ 
+void arena_create(int **arena) {
+    *arena = malloc(1024);
+}
 
 int main() {
     /*int *c = malloc(sizeof(int));
@@ -23,7 +27,7 @@ int main() {
     // Lets do our arena!
     // Should be able to fit 256 integer pointers in a big chunk here. 
     // Alternativley, can only fit 128 integers. Lets try with that instead.
-    printf("Int size = %lu, int pointer size = %lu\n", sizeof(int), sizeof(int*));
+    /*printf("Int size = %lu, int pointer size = %lu\n", sizeof(int), sizeof(int*));
     int *arena = malloc(1024);
     printf("\nAllocated 'arena'. Memory address = %p\n\n", arena);
     int *test = arena; 
@@ -46,10 +50,24 @@ int main() {
     printf("Address of arena++ = %p, Value at arena ++ = %d\n", (void *) arena2, *arena2);
     arena2++;
     printf("Address of arena++ again = %p, Value at arena++ again = %d\n", (void *) arena2, *arena2);
-    bool check = (arena2 == test);
+    bool check = (arena2 ==     test);
     printf("%d\n", check);
+
+*/
+
+
+    //int *test2 = NULL;
+    //printf("test2 addy = %p\n", test2);
+    //test2 = malloc(1024);
+    //printf("test2 post alloc addy = %p\n", test2);
+
+    int *test = NULL;
+    printf("\ntest = %p", test);
+    arena_create(&test);
+    printf("\nval = %p", test);
 
 
 
     return 0;
 }
+
